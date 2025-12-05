d9f04bff42e74ca2de77a38b1596ea7f5509b5f3
Rodrigo Gomes <rodrigogms.pereira@hotmail.com>
2025-12-05 03:15:59 -0300

    feat: Adding tests and making some small modifications

--- src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -10,7 +10,11 @@ public class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>
     {
         RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceMessagesExeption.NAME_EMPTY);
         RuleFor(user => user.Email).NotEmpty().WithMessage(ResourceMessagesExeption.EMAIL_EMPTY);
-        RuleFor(user => user.Email).EmailAddress().WithMessage(ResourceMessagesExeption.EMAIL_INVALID);
-        RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6);
+        RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6).WithMessage(ResourceMessagesExeption.PASSWORD_EMPTY);
+        When(user => string.IsNullOrEmpty(user.Email) == false, () =>
+            {
+                RuleFor(user => user.Email).EmailAddress().WithMessage(ResourceMessagesExeption.EMAIL_INVALID);
+            }
+        );
     }
 }
--- src/Backend/MyRecipeBook.Infrastructure/DependencyInjectionExtension.cs
+++ src/Backend/MyRecipeBook.Infrastructure/DependencyInjectionExtension.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using FluentMigrator.Runner;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -5,6 +7,7 @@ using MyRecipeBook.Domain.Repositories;
 using MyRecipeBook.Domain.Repositories.User;
 using MyRecipeBook.Infrastructure.DataAccess;
 using MyRecipeBook.Infrastructure.DataAccess.Repositories;
+using MyRecipeBook.Infrastructure.Extensions;
 
 namespace MyRecipeBook.Infrastructure;
 
@@ -12,16 +15,21 @@ public static class DependencyInjectionExtension
 {
     public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        AddDbContext(services,configuration);
         AddRepositories(services);
+        if (configuration.IsUnitTestEnvironment())
+            return;
+        
+        AddDbContext(services,configuration);
+        AddFluentMigrator(services, configuration);
+        
     }
 
     private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
     {
         
-        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        //var connectionString = configuration.GetConnectionString("DefaultConnection");
         
-        services.AddDbContext<MyRecipeBookDbContext>(dbContextOptions => dbContextOptions.UseSqlServer(connectionString));
+        services.AddDbContext<MyRecipeBookDbContext>(dbContextOptions => dbContextOptions.UseSqlServer(configuration.ConnectionString()));
     }
     
     private static void AddRepositories(IServiceCollection services)
@@ -31,6 +39,16 @@ public static class DependencyInjectionExtension
         services.AddScoped<IUserReadOnlyRepository,UserRepository>();
         services.AddScoped<IUserWriteOnlyRepository,UserRepository>();
     }
+
+    private static void AddFluentMigrator(IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.ConnectionString();
+        services.AddFluentMigratorCore().ConfigureRunner(options => options.
+            AddSqlServer().
+            WithGlobalConnectionString(connectionString)
+            .ScanIn(Assembly.Load("MyRecipeBook.Infrastructure")).For.All());
+    }
+
 
 
 }
